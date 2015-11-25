b0c6fe437f35d9bfad715d2f7e8a8a6a51867ad2
Jason Liu <jason@jasonkliu.com>
2015-11-25 11:40:18 -0800

    Create an event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,14 @@ class EventsController < ApplicationController
   def new
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   def create
-    render plain: params[:event].inspect
+    @event = Event.new(params.require(:event).permit(:number, :text))
+
+    @event.save
+    redirect_to @event
   end
 end
